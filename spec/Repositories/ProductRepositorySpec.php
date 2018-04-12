1658714805c1e61a1b181259d455662712f0af35
mp <mariusz.piatek92@gmail.com>
2018-04-12 18:59:40 +0200

    Added spec for getProductsWithAmountOver function of the ProductsRepository.

--- spec/Repositories/ProductRepositorySpec.php
+++ spec/Repositories/ProductRepositorySpec.php
@@ -6,6 +6,7 @@ namespace spec\MrPiatek\BlueServer\Repositories;
 use Illuminate\Container\Container as App;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Collection;
+use MrPiatek\BlueServer\Exceptions\InvalidAmountException;
 use MrPiatek\BlueServer\Interfaces\ProductsRepositoryInterface;
 use MrPiatek\BlueServer\Models\Product;
 use MrPiatek\BlueServer\Entities\Product as ProductEntity;
@@ -19,6 +20,7 @@ class ProductRepositorySpec extends ObjectBehavior
         ['id' => 1, 'name' => 'Laptop', 'amount' => 5],
         ['id' => 2, 'name' => 'PC', 'amount' => 3],
         ['id' => 3, 'name' => 'MacBook', 'amount' => 0],
+        ['id' => 4, 'name' => 'iPhone', 'amount' => 7],
     ];
 
     function let(App $app, Product $productModel, Builder $query)
@@ -90,5 +92,42 @@ class ProductRepositorySpec extends ObjectBehavior
         }
 
         $this->getProductsOutOfStock()->shouldBeLike($expectedResult);
+    }
+
+    function it_gets_products_with_amount_over(Product $productModel, Builder $query)
+    {
+        $amountOverValue = 5;
+        for ($i = 1; $i <= $amountOverValue; $i++) {
+            $products = array_filter(self::PRODUCTS_IN_DATABASE, function ($product) use ($i) {
+                return $product['amount'] > $i;
+            });
+
+            $productModel->newQuery()->shouldBeCalled();
+
+            $query->where('amount', '>', $i)
+                ->shouldBeCalled()
+                ->willReturn($query);
+            $query->get()
+                ->shouldBeCalled()
+                ->willReturn(new Collection($products));
+
+            $expectedResult = [];
+            foreach ($products as $product) {
+                $expectedResult[] = new ProductEntity(
+                    $product['id'],
+                    $product['name'],
+                    $product['amount']
+                );
+            }
+
+            $this->getProductsWithAmountOver($i)
+                ->shouldBeLike($expectedResult);
+        }
+    }
+
+    function it_throws_exception_on_invalid_amount()
+    {
+        $this->shouldThrow(InvalidAmountException::class)
+        ->duringGetProductsWithAmountOver(-1);
     }
 }
